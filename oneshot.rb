e0d9634b594b1f8c90f6f319852649a563026399
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-14 20:18:19 -0700

    Brew formula update for oneshot version v1.1.3

--- oneshot.rb
+++ oneshot.rb
@@ -2,24 +2,24 @@
 class Oneshot < Formula
   desc "A single fire HTTP server."
   homepage "https://github.com/raphaelreyna/oneshot"
-  version "1.1.2"
+  version "1.1.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/raphaelreyna/oneshot/releases/download/v1.1.2/oneshot_1.1.2.macos-x86_64.tar.gz"
-    sha256 "46fc5b787427e6ce4695c0c95624bcb9ebbce19179f485da3a20db07cca5707f"
+    url "https://github.com/raphaelreyna/oneshot/releases/download/v1.1.3/oneshot_1.1.3.macos-x86_64.tar.gz"
+    sha256 "812eeb9787ae2d90c86cf946441d5b0bdd1b240c41644bbc9e71d2c1bdc2de2a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/raphaelreyna/oneshot/releases/download/v1.1.2/oneshot_1.1.2.linux-x86_64.tar.gz"
-      sha256 "ce5bf39c31795ef2a166e31bf690c79c0ded13f3975a6e490a2d3dc269a51c24"
+      url "https://github.com/raphaelreyna/oneshot/releases/download/v1.1.3/oneshot_1.1.3.linux-x86_64.tar.gz"
+      sha256 "2cc34898b9ac2852e225f65c0158acc3534448f10603928f4d3bcca2cfcb7cee"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/raphaelreyna/oneshot/releases/download/v1.1.2/oneshot_1.1.2.linux-arm64.tar.gz"
-        sha256 "fea9374add8b934c76e36e0c102e0d60c99ed0938260cfe4bae7c9ef88755b61"
+        url "https://github.com/raphaelreyna/oneshot/releases/download/v1.1.3/oneshot_1.1.3.linux-arm64.tar.gz"
+        sha256 "368a7042c60932b332ef09516e4b940cedf35c79f59ba5744a3c7fb9a267dd90"
       else
-        url "https://github.com/raphaelreyna/oneshot/releases/download/v1.1.2/oneshot_1.1.2.linux-arm.tar.gz"
-        sha256 "310e32405df3c6e0655685a0642a5b9b9768f35a5ec5b36976f21364dc200bea"
+        url "https://github.com/raphaelreyna/oneshot/releases/download/v1.1.3/oneshot_1.1.3.linux-arm.tar.gz"
+        sha256 "9bff290c86409cc5f5a6f2b0586140e4cd83bcb0b9e5413d6f16156b5a65c5e1"
       end
     end
   end
